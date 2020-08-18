9eaf75bba9c90dd9bd86619ebaaad5a83d6eebd6
Otto Söderman <ottoj.soderman@gmail.com>
2020-08-18 14:04:28 +0300

    frontend kinda finally works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_DRINKS } from './queries'
+import DrinkCard from './components/DrinkCard'
+import CardGroup from 'react-bootstrap/CardGroup'
+
 
 function App() {
   const result = useQuery(ALL_DRINKS)
-  console.log(result)
   if (!result.data || result.loading) {
     return null
   }
   const drinks = result.data.allDrinks
 
-  console.log(drinks)
+
+  const groupByN = (data, n) => {
+    let result = []
+    for (let i = 0; i < data.length; i += n) result.push(data.slice(i, i + n))
+    return result;
+  }
+
+  const groupedDrinks = groupByN(drinks.slice(12000, drinks.length).map((drink, i) => (
+    <DrinkCard style={{ display: 'inline-block' }} key={drink.id} drink={drink}></DrinkCard>
+  )), 3)
+
   return (
     <div className="container">
       <h1>Drinks</h1>
-        {
-          drinks.map(drink => (
-            <li key={drink.id}>{drink.name}</li>
-          ))
-        }
+      {
+        groupedDrinks.map(group => <CardGroup key={group[0].key}>{group}</CardGroup>)
+      }
     </div>
   );
 }
--- frontend/src/components/DrinkCard.js
+++ frontend/src/components/DrinkCard.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import Card from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
 
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+const DrinkCard = ({ drink }) => {
+
+    const capitalizeFirst = string => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    return (
+        <Card style={{ display: 'inline-block', width: "33%" }}>
+            <Card.Body>
+                <Card.Title style={{ height: "5rem" }}>{drink.name}</Card.Title>
+                <Card.Subtitle>{capitalizeFirst(drink.category)}</Card.Subtitle>
+                <Card.Img variant="top" src={drink.imageLink} style={{ maxWidth: "50%", maxHeight: "20rem", width: "auto", height: "auto" }} />
+                <Card.Text style={{ display: 'inline-block', padding: "5%" }}>
+                    {drink.price}€<br></br>
+                    {drink.percentage}%<br></br>
+                    {drink.size}l<br></br>
+                    {capitalizeFirst(drink.store)}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default DrinkCard
