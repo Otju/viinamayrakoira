6f5c672f5816f6b01bf7a5dad1d44babd1cce3d1
Otto Söderman <ottoj.soderman@gmail.com>
2020-08-26 00:47:02 +0300

    styling

--- frontend/src/components/SearchVariableMenu.js
+++ frontend/src/components/SearchVariableMenu.js
@@ -88,12 +88,10 @@ const SearchVariableMenu = ({ searchVariables, setSearchVariables }) => {
             <Dropdown.Menu>
               {minMaxItems.map(item => (
                 <Dropdown.ItemText key={item.name}>
-                  <label>
-                    {item.displayName}
-                    <Field type="number" min="0" max={searchVariables[`max${item.name}`] ? searchVariables[`max${item.name}`]-1 : null} name={`min${item.name}`} style={{ display: "inline-block", width: "5rem" }} placeholder="min" className="form-control" />
+                  <span style={{ width: "6rem", display: "inline-block" }}>{item.displayName}</span>
+                  <Field type="number" min="0" max={searchVariables[`max${item.name}`] ? searchVariables[`max${item.name}`] - 1 : null} name={`min${item.name}`} style={{ display: "inline-block", width: "5rem" }} placeholder="min" className="form-control" />
                     -
-                    <Field type="number" min={searchVariables[`min${item.name}`] ? searchVariables[`min${item.name}`]+1 : 0} name={`max${item.name}`} style={{ display: "inline-block", width: "5rem" }} placeholder="max" className="form-control" />
-                  </label>
+                  <Field type="number" min={searchVariables[`min${item.name}`] ? searchVariables[`min${item.name}`] + 1 : 0} name={`max${item.name}`} style={{ display: "inline-block", width: "5rem" }} placeholder="max" className="form-control" />
                 </Dropdown.ItemText>
               ))}
             </Dropdown.Menu>
