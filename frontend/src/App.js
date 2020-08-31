08716a56e4c78741a61a9e9800013ebf8e450c37
Otto Söderman <ottoj.soderman@gmail.com>
2020-08-31 20:47:33 +0300

    implemented react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import SearchVariableMenu from './components/SearchVariableMenu/'
 import DrinkCardList from './components/DrinkCardList'
 import NavigationBar from './components/NavigationBar'
-import { Switch, Route, Link, } from "react-router-dom"
+import { Switch, Route,Redirect  } from "react-router-dom"
 
 
 const App = () => {
@@ -13,9 +13,21 @@ const App = () => {
     <div className="container" style={{ background: "#ede8e8" }}>
       <NavigationBar></NavigationBar>
       <Switch>
-        <Route path="/">
+        <Route path="/statistics">
+          <h1>Statistics</h1>
+        </Route>
+        <Route path="/bars">
+          <h1>Baarit</h1>
+        </Route>
+        <Route path="/moreinfo">
+          <h1>Lisää infoa</h1>
+        </Route>
+        <Route path="/drinks">
           <SearchVariableMenu searchVariables={searchVariables} setSearchVariables={setSearchVariables} />
           <DrinkCardList searchVariables={searchVariables} />
+        </Route>
+        <Route path="/">
+          <Redirect to="/drinks"/>
         </Route>
       </Switch>
     </div>
--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { LinkContainer } from "react-router-bootstrap"
 
 const NavigationBar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href=" "><img src={process.env.PUBLIC_URL + '/doggo.svg'} style={{ height: "5rem", fill: "red" }} alt="viinamayrakoira.svg" /></Navbar.Brand>
+      <LinkContainer to="/drinks">
+        <Navbar.Brand href=" ">
+          <img src={process.env.PUBLIC_URL + '/doggo.svg'} style={{ height: "5rem", fill: "red" }} alt="viinamayrakoira.svg" />
+        </Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#statistics">Statistiikkaa</Nav.Link>
-          <Nav.Link href="#moreinfo">Lisää infoa</Nav.Link>
+          <LinkContainer to="/drinks"><Nav.Link>Juomat</Nav.Link></LinkContainer>
+          <LinkContainer to="/statistics"><Nav.Link>Statistiikkaa</Nav.Link></LinkContainer>
+          <LinkContainer to="/bars"><Nav.Link>Baarit</Nav.Link></LinkContainer>
+          <LinkContainer to="/moreinfo"><Nav.Link>Lisää infoa</Nav.Link></LinkContainer>
         </Nav>
         <Navbar.Brand>Viinamayrakoira.fi</Navbar.Brand>
       </Navbar.Collapse>
