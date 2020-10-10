34811f168e41cd2693eb6c76168da622570c8e76
Otto Söderman <ottoj.soderman@gmail.com>
2020-10-10 14:26:28 +0300

    disabled Bars tab

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 import { LinkContainer } from "react-router-bootstrap"
 
 const NavigationBar = () => {
@@ -14,12 +16,12 @@ const NavigationBar = () => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-        <LinkContainer to="/best"><Nav.Link>Parhaat</Nav.Link></LinkContainer>
+          <LinkContainer to="/best"><Nav.Link>Parhaat</Nav.Link></LinkContainer>
           <LinkContainer to="/drinks"><Nav.Link>Juomat</Nav.Link></LinkContainer>
-          <LinkContainer to="/statistics"><Nav.Link>Tilastoja</Nav.Link></LinkContainer>
-          <LinkContainer to="/bars"><Nav.Link>Baarit</Nav.Link></LinkContainer>
+          <OverlayTrigger overlay={<Tooltip>Vielä työn alla</Tooltip>}><div><LinkContainer to="/bars"><Nav.Link disabled>Baarit</Nav.Link></LinkContainer></div></OverlayTrigger>
           <LinkContainer to="/portioncalculator"><Nav.Link>Annoslaskuri</Nav.Link></LinkContainer>
-          <LinkContainer to="/moreinfo"><Nav.Link>Lisää infoa</Nav.Link></LinkContainer>
+          <LinkContainer to="/statistics"><Nav.Link>Tilastoja</Nav.Link></LinkContainer>
+          <LinkContainer to="/moreinfo"><Nav.Link>Tietoa</Nav.Link></LinkContainer>
         </Nav>
         <Navbar.Brand>Viinamayrakoira.fi</Navbar.Brand>
       </Navbar.Collapse>
