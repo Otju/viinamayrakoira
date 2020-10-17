0ffb99177d0e12133f4d4d80fef1191f6a319c12
Otto Söderman <ottoj.soderman@gmail.com>
2020-10-17 13:56:53 +0300

    Statistics and InfoPage basic tests

---
+++ frontend/cypress/integration/statAndInfoPage.test.js
@@ -0,0 +1,12 @@
+it('StatististicsPage opens ', function () {
+  cy.visit('http://localhost:3000/statistics')
+  cy.contains("Juomia yhteensä")
+  cy.contains("Keskimääräinen annoshinta kaupoittain")
+})
+
+it('StatististicsPage opens ', function () {
+  cy.visit('http://localhost:3000/moreinfo')
+  cy.contains("Lahjoitukset")
+  cy.contains("Sivut")
+  cy.contains("Juomat")
+})
